f1e34d4b00735bbeab3eecdc75b0f3e36577ae65
Robert Jones <robert.g.jones@gmail.com>
2017-06-14 15:25:52 +0100

    guard for y coord choice

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -18,8 +18,13 @@ describe("A Player", function() {
     });
 
     describe("when picking coords, ", function() {
-      it("can't pick an x-coord greater than 3", function() {
+      it("can't pick an x-coord 1 > x > 3", function() {
         expect(function() { player.placeMarker(4,1) }).toThrow("Invalid coordinate choice");
+        expect(function() { player.placeMarker(-1,1) }).toThrow("Invalid coordinate choice");
+      });
+      it("can't pick an y-coord 1 > y > 3", function() {
+        expect(function() { player.placeMarker(3,4) }).toThrow("Invalid coordinate choice");
+        expect(function() { player.placeMarker(3,0) }).toThrow("Invalid coordinate choice");
       });
     });
   });
